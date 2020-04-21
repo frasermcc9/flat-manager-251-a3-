a2b6130970d3e4cd098fe4d53c565e092056b831
Fraser McCallum <fraser@themccallums.net.nz>
2020-04-21 19:03:05 +1200

    Added support for multiple sort keys, as well as a format comparison

--- item/ItemComparator.java
+++ item/ItemComparator.java
@@ -2,31 +2,54 @@ package item;
 
 import java.util.Comparator;
 
-//TODO change to not accessing protected variables?
-
+/**
+ * a comparison enum which compares objects that extend the Item class. 
+ * 
+ */
 public enum ItemComparator implements Comparator<Item> {
     Title {
         @Override
         public int compare(Item o1, Item o2) {
-            return o1._title.compareTo(o2._title);
+            return o1.getTitle().compareTo(o2.getTitle());
         }
     },
     Creator {
         @Override
         public int compare(Item o1, Item o2) {
-            return o1._creator.compareTo(o2._creator);
+            return o1.getCreator().compareTo(o2.getCreator());
         }
     },
     Acquisition {
         @Override
         public int compare(Item o1, Item o2) {
-            return o1._acquireDate.compareTo(o2._acquireDate);
+            return o1.getAcquireDate().compareTo(o2.getAcquireDate());
         }
     },
     Owner {
         @Override
         public int compare(Item o1, Item o2) {
-            return o1._owner.compareTo(o2._owner);
+            return o1.getOwner().compareTo(o2.getOwner());
         }
+    },
+    Format {
+        @Override
+        public int compare(Item o1, Item o2) {
+            return o1.getClass().getName().compareTo(o2.getClass().getName());
+        }
+    };
+
+    public static Comparator<Item> orderSort(ItemComparator... itemComparators) {
+        return new Comparator<Item>() {
+            @Override
+            public int compare(Item o1, Item o2) {
+                for (ItemComparator sortKey : itemComparators) {
+                    int result = sortKey.compare(o1, o2);
+                    if (result != 0) {
+                        return result;
+                    }
+                }
+                return 0;
+            }
+        };
     }
 }
