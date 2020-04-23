083cacc43c697b89d0a2f50ef6c572a5f03d0909
Fraser McCallum <fraser@themccallums.net.nz>
2020-04-23 21:12:31 +1200

    Major refactoring of most code into the InventoryCollection class

--- inventoryman/InventoryManImpl.java
+++ inventoryman/InventoryManImpl.java
@@ -1,35 +1,26 @@
 package inventoryman;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
-import item.AbstractItem;
 import item.Book;
-import item.ItemComparator;
 import item.Music;
 
 public class InventoryManImpl implements InventoryMan {
 
     private String _flatName;
-    private InventoryCollection _items = new InventoryCollection();
-    // private Map<String, AbstractItem> _items = new HashMap<String,
-    // AbstractItem>();
+    private InventoryCollection _flatCollection = new InventoryCollection();
 
     public InventoryManImpl(String flatName) {
         _flatName = flatName;
     }
 
     @Override
-    public String addBook(String author, String title, String publicationYear, String publisher,
-            String acquisitionDateStr, String owner, String costStr, String formatStr) {
+    public String addBook(String author, String title, String pubYear, String publisher, String acquisitionDateStr,
+            String owner, String costStr, String formatStr) {
         try {
-            return _items.newBook(author, title, publicationYear, publisher, acquisitionDateStr, owner, costStr,
-                    formatStr);
+            Book book = new Book(author, title, pubYear, publisher, acquisitionDateStr, owner, costStr, formatStr);
+            return _flatCollection.itemHandler(book);
         } catch (Exception e) {
             return "ERROR " + e.getMessage();
         }
@@ -39,14 +30,8 @@ public class InventoryManImpl implements InventoryMan {
     public String addMusic(String artist, String title, String releaseDateStr, String acquisitionDateStr, String owner,
             String costStr, String formatStr) {
         try {
-            Music newMusic = new Music(artist, title, releaseDateStr, acquisitionDateStr, owner, costStr, formatStr);
-
-            if (_items.containsKey(newMusic.getId()))
-                throw new DuplicateItemException("Duplicate item added to inventory:" + newMusic.getId());
-
-            _items.put(newMusic.getId(), newMusic);
-
-            return "Success";
+            Music music = new Music(artist, title, releaseDateStr, acquisitionDateStr, owner, costStr, formatStr);
+            return _flatCollection.itemHandler(music);
         } catch (Exception e) {
             return "ERROR " + e.getMessage();
         }
@@ -54,65 +39,36 @@ public class InventoryManImpl implements InventoryMan {
 
     @Override
     public String getItemToDisplay(String creator, String title, String formatStr) {
-        String id = AbstractItem.getId(creator, title, formatStr);
-        AbstractItem retrievedItem = _items.get(id);
-        return retrievedItem.getDetails();
+        try {
+            return _flatCollection.findByProperties(creator, title, formatStr).getDetails();
+        } catch (Exception e) {
+            return e.getMessage();
+        }
     }
 
     @Override
-    public List<String> getAll(String order) throws IllegalArgumentException {
-        List<AbstractItem> items = new ArrayList<AbstractItem>(_items.values());
-        ItemComparator key = ItemComparator.valueOf(order);
-
-        Collections.sort(items, key);
-
-        List<String> output = new ArrayList<String>();
-        for (AbstractItem a : items) {
-            output.add(a.getDetails());
+    public List<String> getAll(String order) {
+        try {
+            return _flatCollection.getItemDetailsByOrder(order);
+        } catch (Exception e) {
+            return _flatCollection.getItemDetailsByOrder("Title"); // if an invalid key given, then just order by title.
         }
-        return output;
     }
 
     @Override
     public List<String> getItemsAcquiredInYear(String year) {
-        List<AbstractItem> items = new ArrayList<AbstractItem>(_items.values());
-        ItemComparator key = ItemComparator.Acquisition;
-
-        Collections.sort(items, key);
-
-        List<String> output = new ArrayList<String>();
-        for (AbstractItem a : items) {
-            if (a.checkIfAcquiredInYear(year)) {
-                output.add(a.getDetails());
-            }
-        }
-        return output;
+        return _flatCollection.getItemDetailsAcquiredIn(year);
     }
 
     @Override
     public List<String> getCreators() {
-        Set<String> creators = new HashSet<String>();
-        for (AbstractItem a : new ArrayList<AbstractItem>(_items.values()))
-            creators.add(a.getCreator());
-        List<String> output = new ArrayList<String>(creators);
+        List<String> output = _flatCollection.getCreators();
         Collections.sort(output);
         return output;
     }
 
     @Override
     public List<String> getFlatReport() {
-        List<String> output = new ArrayList<String>();
-        List<AbstractItem> items = new ArrayList<AbstractItem>(_items.values());
-
-        output.add(_flatName);
-
-        Collections.sort(items, ItemComparator.orderSort(ItemComparator.Owner, ItemComparator.Format,
-                ItemComparator.Creator, ItemComparator.Title));
-
-        for (AbstractItem a : items) {
-            output.add(a.formatReport());
-        }
-
-        return output;
+        return _flatCollection.inventoryReport(_flatName);
     }
 }
