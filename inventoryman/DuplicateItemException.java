04cde5eaa4cfc87087b753a577b895d5a6056bd0
Fraser McCallum <fraser@themccallums.net.nz>
2020-04-25 16:29:05 +1200

    Added header comments to explain intended exception usage.

--- inventoryman/DuplicateItemException.java
+++ inventoryman/DuplicateItemException.java
@@ -1,7 +1,10 @@
 package inventoryman;
 
+/**
+ * Thrown when a duplicate item is added to the collection
+ */
 public class DuplicateItemException extends Exception {
-    public DuplicateItemException(String errorMessage){
+    public DuplicateItemException(String errorMessage) {
         super(errorMessage);
     }
 }
--- item/InvalidFormatException.java
+++ item/InvalidFormatException.java
@@ -1,5 +1,8 @@
 package item;
 
+/**
+ * Thrown when a provided argument is of an invalid format.
+ */
 public class InvalidFormatException extends Exception {
     public InvalidFormatException(String errorMessage) {
         super(errorMessage);
